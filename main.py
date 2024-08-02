bad9083439bbd25df2f6cd076ac0db783dd50928
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-08-02 13:45:14 -0700

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-# with open('weather_data.csv', 'r') as data_file:
-#     # data = data_file.readlines()
-#     # print(data)
-#     for line in data_file:
-#         print(line.strip())
+import pandas
+import numpy as np
 
-import csv
-with open('weather_data.csv', 'r') as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
+# Load the data from the CSV file
+data = pandas.read_csv("weather_data.csv")
+df = pandas.DataFrame(np.random.randn(5,3),columns=['Monday','temp','condition'])
 
-    for row in data:
-        print(row)
+##################################################################
+# # Output: <class 'pandas.core.frame.DataFrame'>
+# # Dataframe is equivalent to your entire table.
+# print(type(data))
+#
+# # Output: <class 'pandas.core.series.Series'>
+# # Series is essentially a column.
+# print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+##################################################################
+
+# # Convert the "temp" column to a list
+# temp_list = data["temp"].to_list()
+# # print(temp_list)
+#
+# len_list = (len(temp_list))
+# # print(len_list)
+
+##################################################################
+# # Calculate the sum of the list using a for loop
+# total_sum = 0
+# for x in len_list:
+#     total_sum += x
+
+# # Calculate the sum of the list using the built-in sum() function
+# total_sum = sum(temp_list)
+# average = total_sum / len_list
+# print(average)
+
+# # Calculate the sum of the list using the built-in mean/median/mode() function
+# print(data.temp.mean())
+# print(data.temp.median())
+# print(data.temp.mode())
+# print(data.temp.max())
+##################################################################
+
+# # Get data in columns
+# print(data["condition"])
+# print(data.condition)
+
+# # Get data in row
+# print(data[data.day == "Monday"])
+
+# Print row with max temperature
+print(data[data.temp == data.temp.max()])
