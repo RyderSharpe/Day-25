914bed1a61fe3a198080913d2b04987664d85118
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-08-05 16:08:56 -0700

    Update class_main.py

--- class_main.py
+++ class_main.py
@@ -1,75 +1,44 @@
+import turtle
 import pandas
 
-# Load the data from the CSV file
-data = pandas.read_csv("weather_data.csv")
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
-#################################################################
-# Output: <class 'pandas.core.frame.DataFrame'>
-# Dataframe is equivalent to your entire table.
-# print(type(data))
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+# print(all_states)
+guessed_states = []
 
-# Output: <class 'pandas.core.series.Series'>
-# Series is essentially a column.
-# print(type(data["temp"]))
+while len(guessed_states) < 50:
 
-# data_dict = data.to_dict()
-# print(data_dict)
-# #################################################################
-#
-# # Convert the "temp" column to a list
-# temp_list = data["temp"].to_list()
-# # print(temp_list)
-#
-# len_list = (len(temp_list))
-# # print(len_list)
-#
-# #################################################################
-# # Calculate the sum of the list using a for loop
-# total_sum = 0
-# for x in len_list:
-#     total_sum += x
-#
-# # Calculate the sum of the list using the built-in sum() function
-# total_sum = sum(temp_list)
-# average = total_sum / len_list
-# print(average)
-#
-# # Calculate the sum of the list using the built-in mean/median/mode() function
-# print(data.temp.mean())
-# print(data.temp.median())
-# print(data.temp.mode())
-# print(data.temp.max())
-# #################################################################
-#
-# # GET DATA IN COLUMNS
-# print(data["condition"])
-print(data.condition)
-#
-# # GET DATA IN ROW
-# print(data[data.day == "Monday"])
-# # Print row with max temperature
-# print(data[data.temp == data.temp.max()])
-# #################################################################
-#
-# # My version #
-# monday = data[data.day == "Monday"]
-# # print(monday.condition)
-# print(monday.temp)
-# temp_fahrenheit = ((monday.temp) * 9/5) + 32
-# print(temp_fahrenheit)
-#
-# # Class version #
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
-# monday_temp_f = monday_temp * 9/5 + 32
-# print(monday_temp_f)
-# ##################################################################
-#
-# ## CREATE A DATAFRAME FROM SCRATCH
-# data_dict = {
-#     "students" : ["Amy", "James", "Angela"],
-#     "scores" : [76, 56, 65]
-# }
-# data = pandas.DataFrame(data_dict)
-# # print(data)
-# data.to_csv("new_data.csv")
+    # User input & input display & score.
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+
+    if answer_state == "Exit":
+        missing_states = []
+
+        # List of missing states
+        for state in all_states:
+
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+
+        # Exit while loop if answer_state == "Exit":
+        break
+
+    # Write answer in list & on screen.
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
